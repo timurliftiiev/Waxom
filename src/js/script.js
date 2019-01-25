ef34fb12651959a8b34be0cb89483ebc9246ea7f
Timur Liftiiev <timurliftiiev@MacBook-Air-Timur.local>
2019-01-25 20:57:35 +0200

    changed conf for loader

--- src/js/script.js
+++ src/js/script.js
@@ -1,97 +1,96 @@
 "use strict";
+$(document).ready(function() {
+  
+  // Loader
+  var $loader = $(".loader");
+  var $scale = $(".loader__scale");
+  var $percentage = $(".loader__perc");
+  var $images = $("img");
+  var $imagesAmount = $images.length;
+  var $imagesLoaded = 0;
 
-// Loader
-var $loader = $('.loader');
-var $scale = $('.loader__scale');
-var $percentage = $('.loader__perc');
-var $images = $('img');
-var $imagesAmount = $images.length;
-var $imagesLoaded = 0;
-
-
-
-for (var i = 0; i < $imagesAmount; i++) {
-  var imageClone = new Image();
-      imageClone.onload = imageLoaded;
-      imageClone.onerror = imageLoaded;
-      imageClone.src = $images[i].src;
-}
-
-function imageLoaded() {
-  $imagesLoaded++;
-
-  var $perc = Math.round((100 / $imagesAmount) * $imagesLoaded);
-
-  $percentage.html($perc);
-  $scale.css("width", $perc + '%');
-
-  if ($imagesLoaded >= $imagesAmount) {
-      setTimeout(function() {
-                  if (!$loader.hasClass('js-hidden')) {
-                      $loader.addClass('js-hidden');
-                  }
-              }, 1000);
+  for (var i = 0; i < $imagesAmount; i++) {
+    var imageClone = new Image();
+    imageClone.onload = imageLoaded;
+    imageClone.onerror = imageLoaded;
+    imageClone.src = $images[i].src;
   }
-};
 
-$(document).ready(function(){
+  function imageLoaded() {
+    $imagesLoaded++;
+
+    var $perc = Math.round((100 / $imagesAmount) * $imagesLoaded);
+
+    $percentage.html($perc);
+    $scale.css("width", $perc + "%");
+
+    if ($imagesLoaded >= $imagesAmount) {
+      setTimeout(function() {
+        if (!$loader.hasClass("js-hidden")) {
+          $loader.addClass("js-hidden");
+        }
+      }, 1000);
+    }
+  }
 
   // Sandwich button
-  $('#js-sandwich').click(function() {
+  $("#js-sandwich").click(function() {
     // Button Animation
-    $(this).toggleClass('js-close');
+    $(this).toggleClass("js-close");
 
     // Mobile Menu
-    $('.mobile').toggleClass('js-show');
+    $(".mobile").toggleClass("js-show");
   });
 
   // Active Button Mobile Menu
-  $('.mobile__link').click(function(e) {
-
-    $('.mobile__link').removeClass('js-active');
-    $(e.target).addClass('js-active');
+  $(".mobile__link").click(function(e) {
+    $(".mobile__link").removeClass("js-active");
+    $(e.target).addClass("js-active");
 
     // Remove Classes after click on menu item
-    $('body').removeClass('no-scroll');
-    $('.mobile').removeClass('js-show');
-    $('#js-sandwich').removeClass('js-close');
+    $("body").removeClass("no-scroll");
+    $(".mobile").removeClass("js-show");
+    $("#js-sandwich").removeClass("js-close");
   });
 
   // Slick Slider
-  $('.slider').slick({
-      dots: true,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      pauseOnHover: false,
-      speed: 1000
-    });
-
-    // MixitUP
-    $('.portfolio__list').mixItUp();
-
-    // Active Button Portfolio
-    $(".portfolio__btn").click(function() {
-  		$(".portfolio__btn").removeClass("portfolio__btn--active");
-  		$(this).addClass("portfolio__btn--active");
-    }); 
-
-    // Scroll
-    $("a").on('click', function(event) {
-
-      if (this.hash !== "") {
-        event.preventDefault();
-
-        var hash = this.hash;
-
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1000, function(){
-          window.location.hash = hash;
-        });
-      } 
+  $(".slider").slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    speed: 1000
   });
-// End Scroll
+
+  // MixitUP
+  $(".portfolio__list").mixItUp();
+
+  // Active Button Portfolio
+  $(".portfolio__btn").click(function() {
+    $(".portfolio__btn").removeClass("portfolio__btn--active");
+    $(this).addClass("portfolio__btn--active");
+  });
+
+  // Scroll
+  $("a").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        1000,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+  // End Scroll
 
   // WOW
   new WOW().init();
